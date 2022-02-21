4756e15d4c7b86804b5a665fe8a141bb9fdc9ce4
Médéric Bélec <1950570@cmontmorency.qc.ca>
2022-02-21 13:26:53 -0500

    TP 06

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { RouterModule } from '@angular/router';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccueilComponent,
+    ProblemeComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path:'accueil', component:AccueilComponent},
+      { path:'', redirectTo:'accueil', pathMatch: 'full'},
+      { path:'**', redirectTo:'accueil', pathMatch: 'full'}, // Si le route est inexistante, rediriger l'utilisateur vers accueil
+      { path: 'probleme', component: ProblemeComponent}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
