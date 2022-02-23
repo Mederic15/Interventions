5c1126f0a725ff66aeaec5e6aaa5aef4c03ac7f7
Médéric Bélec <1950570@cmontmorency.qc.ca>
2022-02-23 16:26:34 -0500

    1er test unitaire prénom

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
 
@@ -8,6 +9,7 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ ProblemeComponent ]
     })
     .compileComponents();
@@ -19,7 +21,14 @@ describe('ProblemeComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+  // it('should create', () => {
+  //   expect(component).toBeTruthy();
+  // });
+
+  it("#1 | Zone PRÉNOM invalide avec 2 caractèress", () =>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(2));
+    let errors = zone.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+    });
 });
