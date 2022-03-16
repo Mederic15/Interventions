0d80666034333735dff1544cfd0e2c5102d34daf
Médéric Bélec <1950570@cmontmorency.qc.ca>
2022-03-16 16:25:29 -0400

    tp 10

--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { VerifierCaracteresValidator } from '../shared/longueur-minumum/longueur-minimum.component';
+import { ITypeProbleme } from './probleme';
+import { TypeproblemeService } from './typeprobleme.service';
 
 @Component({
   selector: 'Inter-probleme',
@@ -9,14 +11,22 @@ import { VerifierCaracteresValidator } from '../shared/longueur-minumum/longueur
 })
 export class ProblemeComponent implements OnInit {
   problemeForm: FormGroup;
-  
-
-  constructor(private fb: FormBuilder) { }
+  typesProbleme: ITypeProbleme[];
+  errorMessage: string;
+  constructor(private fb: FormBuilder, private typeproblemeService: TypeproblemeService) { }
 
   ngOnInit(): void {
     this.problemeForm = this.fb.group({
-      prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],        
+      prenom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]], 
+      nom: ['' , [VerifierCaracteresValidator.longueurMinimum(3), Validators.required]],
+      typeprobleme: ['', [Validators.required]]       
     })
+
+    this.typeproblemeService.obtenirTypesProbleme()
+        .subscribe(typesProbleme => this.typesProbleme = typesProbleme,
+                   error => this.errorMessage = <any>error);  
+
+    
   }
 
   save(): void {
